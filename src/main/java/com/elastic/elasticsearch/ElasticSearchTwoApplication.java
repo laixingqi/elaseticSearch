e38995e286ae087ec86e306721a2b81f4fd91557
laixingqi <laixingqi@users.noreply.github.com>
2019-04-10 10:24:15 +0800

    elasticsearchOne

---
+++ src/main/java/com/elastic/elasticsearch/ElasticSearchTwoApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elastic/elasticSearchTwo/ElasticSearchTwoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.elastic.elasticSearchTwo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticSearchTwoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
