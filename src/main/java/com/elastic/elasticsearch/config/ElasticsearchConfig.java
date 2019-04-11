fc9a7741051f974a198ba444f88cb0a65c2e5bea
laixingqi <laixingqi@users.noreply.github.com>
2019-04-11 18:33:52 +0800

    dd

--- src/main/java/com/elastic/elasticsearch/config/ElasticsearchConfig.java
+++ src/main/java/com/elastic/elasticsearch/config/ElasticsearchConfig.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//sdkfjsalkdjfla
+
 @Configuration
 public class ElasticsearchConfig {
 
